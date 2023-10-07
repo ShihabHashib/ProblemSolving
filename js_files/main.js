4a1388add9be35fd0cdb8a03bc3065fb3fc6f0d2
Shihab Hashib <shihabhashib1@gmail.com>
2023-10-07 23:41:28 +0600

    Caesar Cipher Javascript HackerRank Solution

--- js_files/main.js
+++ js_files/main.js
@@ -2,4 +2,4 @@ import { caesarCipher } from "./caesarCipher.js";
 const showResult = document.querySelector(".app");
 
 showResult.innerHTML =
-  "This is result: " + caesarCipher("abcdefghijklmnopqrstuvwxyz", 2);
+  "This is result: " + caesarCipher("abcdefghijklmnopqrstuvwxyz", 3);
