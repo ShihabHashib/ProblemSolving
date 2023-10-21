32619b9ce7f8b7981ff282d54e82a5f810cc6a8f
Shihab Hashib <shihabhashib1@gmail.com>
2023-10-21 11:58:05 +0600

    Python First Test

--- py_problems/first.py
+++ py_problems/first.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(1, "../assets/js/pyscript.js")
 
-# ProblemSolving for LeetCode Python Start
+# ProblemSolving for LeetCode Python Start Here
 
 display("Shihab Hashib")
 print("shihab")
