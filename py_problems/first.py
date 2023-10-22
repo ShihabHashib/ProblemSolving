d4dac67037bddf9b2a7ad53866e67e9babb84df7
Shihab Hashib <shihabhashib1@gmail.com>
2023-10-22 12:51:07 +0600

    First File Changed

--- py_problems/first.py
+++ py_problems/first.py
@@ -3,5 +3,30 @@ sys.path.insert(1, "../assets/js/pyscript.js")
 
 # ProblemSolving for LeetCode Python Start Here
 
-display("Shihab Hashib")
-print("shihab")
+arr = [60, 30, 20, 10, 65, 20, 70, 35, 30, 80, 5]
+
+# Get Sum
+def getSum(arr):
+    total = 0
+    for i in arr:
+        total+=i
+    return total
+
+# Get Minimum
+def getMin(arr):
+    shortest = arr[0]
+    for i in arr:
+        if i < shortest:
+            shortest = i
+    return shortest
+
+# Get Maximum
+def getMax(arr):
+    highest = arr[0]
+    for i in arr:
+        if i > highest:
+            highest = i
+    return highest
+
+
+ 
