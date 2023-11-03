d37d2d65f679e305a7acdfb01fac11fd5ff57ff3
Shihab Hashib <shihabhashib1@gmail.com>
2023-11-03 19:10:14 +0600

    645. Set Mismatch

--- py_problems/645.py
+++ py_problems/645.py
@@ -6,21 +6,40 @@ sys.path.insert(1, "../assets/js/pyscript.js")
 
 
 def findErrorNums(nums):
-    result = set()
-    duplicate = 0
-    missing = 0
 
-    for i in nums:
-        if i in result:
-            duplicate = i
-        result.add(i)
+    n = len(nums)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
 
-    for i in range(1, len(nums) + 1):
-        print(i)
-        if i not in result:
-            missing = i
+    num_set = set()
+    duplicate = None
+    missing = None
+
+    for num in nums:
+        if num in num_set:
+            duplicate = num
+        num_set.add(num)
+
+    missing = expected_sum - actual_sum + duplicate
 
     return [duplicate, missing]
+
+    # First Solution
+    # result = set()
+    # duplicate = 0
+    # missing = 0
+
+    # for i in nums:
+    #     if i in result:
+    #         duplicate = i
+    #     result.add(i)
+
+    # for i in range(1, len(nums) + 1):
+    #     print(i)
+    #     if i not in result:
+    #         missing = i
+
+    # return [duplicate, missing]
 
 
 # END
