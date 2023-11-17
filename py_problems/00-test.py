eb4f5d7cf2b56508eedc6de822ec7c98a8c6bea0
Shihab Hashib <shihabhashib1@gmail.com>
2023-11-17 20:09:36 +0600

    tested

--- py_problems/00-test.py
+++ py_problems/00-test.py
@@ -5,29 +5,4 @@ sys.path.insert(1, "../assets/js/pyscript.js")
 # 00. Test Code
 
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-def funcName(head):
-    slow = fast = head
-
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-
-    return slow
-
-# END
-
-
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(4)
-head.next.next.next.next = ListNode(5)
-
-result = funcName(head)
-display(result)
+print("test")
