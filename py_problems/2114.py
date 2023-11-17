f29cc4c976119816c3fe2df3ae2eb69b7ed057f2
Shihab Hashib <shihabhashib1@gmail.com>
2023-11-17 20:43:17 +0600

    2114. Maximum Number of Words Found in Sentences

---
+++ py_problems/2114.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 2114. Maximum Number of Words Found in Sentences
+
+
+def mostWordsFound(sentences):
+    return max(len(i.split()) for i in sentences)
+
+# END
+
+
+sentences = ["please wait", "continue to fight", "continue to win"]
+result = mostWordsFound(sentences)
+display(result)
