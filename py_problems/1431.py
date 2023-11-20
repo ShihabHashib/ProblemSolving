ccd60cfc3fac0437fae20fbf8356829250cd282d
Shihab Hashib <shihabhashib1@gmail.com>
2023-11-20 20:38:33 +0600

    1431. Kids With the Greatest Number of Candies

---
+++ py_problems/1431.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1431. Kids With the Greatest Number of Candies
+
+
+def kidsWithCandies(candies, extraCandies):
+    result = []
+    greatest = max(candies)
+    for i in candies:
+        totalCandies = i + extraCandies
+        if totalCandies >= greatest:
+            result.append(True)
+        else:
+            result.append(False)
+    print(result)
+
+
+# END
+candies = [2, 3, 5, 1, 3]
+extraCandies = 3
+
+result = kidsWithCandies(candies, extraCandies)
+display(result)
