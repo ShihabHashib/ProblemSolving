5f072557acad7b2ad1dbb9e34a29173a129f7cb4
Shihab Hashib <shihabhashib1@gmail.com>
2023-11-26 16:20:08 +0600

    1720. Decode XORed Array

---
+++ py_problems/1720.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1720. Decode XORed Array
+
+
+def decode(encoded, first):
+
+    target = [first]
+
+    for i in range(len(encoded)):
+        target.append(encoded[i] ^ target[i])
+
+    return target
+
+
+# END
+encoded = [1, 2, 3]
+first = 1
+
+result = decode(encoded, first)
+display(result)
