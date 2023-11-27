2ce182e4b1fdee244e364d07e24d2a5c873400ed
Shihab Hashib <shihabhashib1@gmail.com>
2023-11-27 17:25:36 +0600

    1282. Group the People Given the Group Size They Belong To

---
+++ py_problems/1282.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1282. Group the People Given the Group Size They Belong To
+
+
+def groupThePeople(groupSizes):
+    result = []
+    groups = {}
+
+    for i, size in enumerate(groupSizes):
+        print(i, size)
+        if size not in groups:
+            groups[size] = []
+
+        groups[size].append(i)
+
+        if len(groups[size]) == size:
+            result.append(groups[size])
+            groups[size] = []
+
+    return result
+
+
+# END
+groupSizes = [3, 3, 3, 3, 3, 1, 3]
+
+result = groupThePeople(groupSizes)
+display(result)
