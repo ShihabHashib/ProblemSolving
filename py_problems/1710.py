6b79754bb842ccb8fe8b8d579d0a3dad871a62a0
Shihab Hashib <shihabhashib1@gmail.com>
2023-12-19 06:35:30 +0600

    1710. Maximum Units on a Truck

---
+++ py_problems/1710.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1710. Maximum Units on a Truck
+
+
+def maximumUnits(boxTypes, truckSize):
+    
+
+
+# END
+boxTypes = [[1,3],[2,2],[3,1]]
+truckSize = 4
+
+result = maximumUnits(boxTypes, truckSize)
+display(result)  # imported from pyscript
