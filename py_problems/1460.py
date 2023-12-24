6fde0641ee86d8ec86911e2644f330bd7fe7ae07
Shihab Hashib <shihabhashib1@gmail.com>
2023-12-24 10:14:32 +0600

    2357. Make Array Zero by Subtracting Equal Amounts

---
+++ py_problems/1460.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1460. Make Two Arrays Equal by Reversing Subarrays
+
+
+def canBeEqual(target, arr):
+    
+
+
+# END
+
+target = [1, 2, 3, 4]
+arr = [2, 4, 1, 3]
+result = canBeEqual(target, arr)
+display(result)
