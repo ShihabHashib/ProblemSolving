9635c33cfa6b8a4d27bf061b8d003f763406b722
Shihab Hashib <shihabhashib1@gmail.com>
2023-12-26 07:39:44 +0600

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ py_problems/1689.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+
+
+def minPartitions(n):
+    return int(max(n))
+
+
+# END
+n = "32"
+result = minPartitions(n)
+display(result)
