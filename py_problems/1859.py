d719398b6cf52156e6e92e97c3f6ce03929df2f2
Shihab Hashib <shihabhashib1@gmail.com>
2024-02-28 19:00:07 +0600

    1859. Sorting the Sentence

---
+++ py_problems/1859.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1859. Sorting the Sentence
+
+
+def sortSentence(s):
+    result = ""
+    sortedArr = [i[-1] + i[:-1] for i in s.split()]
+    sortedArr.sort()
+
+    for i in sortedArr:
+        result += i[1:] + " "
+
+    return result
+
+
+# END
+
+
+s = "is2 sentence4 This1 a3"
+
+result = sortSentence(s)
+display(result)
