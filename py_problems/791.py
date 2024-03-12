4746e4d47033d6109bd374d45115ffed850efe5c
Shihab Hashib <shihabhashib1@gmail.com>
2024-03-12 12:24:05 +0600

    791. Custom Sort String

---
+++ py_problems/791.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 791. Custom Sort String
+
+
+def countDigits(order, s):
+    result = ""
+    dic = {}
+    for i in s:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+
+    print(dic)
+
+    for i in order:
+        if i in dic:
+            result += i * dic[i]
+            dic[i] -= 1 * dic[i]
+
+    for i, value in dic.items():
+        if value > 0:
+            result += i * value
+
+    print(dic)
+    return result
+
+
+# END
+order = "kqep"
+s = "pekeq"
+
+result = countDigits(order, s)
+display(result)
