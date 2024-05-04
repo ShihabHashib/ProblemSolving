fbeba0b03324b7a8069fce6266d5f4bcff8fb80c
Shihab Hashib <shihabhashib1@gmail.com>
2024-05-04 23:13:48 +0600

    1688. Count of Matches in Tournament

---
+++ py_problems/1688.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1688. Count of Matches in Tournament
+
+
+def numberOfMatches(n):
+    return n - 1
+
+
+# END
+
+n = 7
+
+result = numberOfMatches(n)
+display(result)
