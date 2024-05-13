16da967cf1a2b1f02a42c01cfebafa3d51b7312a
Shihab Hashib <shihabhashib1@gmail.com>
2024-05-13 20:01:48 +0600

    476. Number Complement

---
+++ py_problems/476.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 476. Number Complement
+
+
+def findComplement(num):
+    binary = "{0:b}".format(num)
+    return int("".join(['1' if char == '0' else '0' for char in str("{0:b}".format(num))]), 2)
+
+
+# END
+num = 1
+
+result = findComplement(num)
+display(result)
