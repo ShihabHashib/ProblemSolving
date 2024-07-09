76c1266ca09c918b6f14c1821018533ef0bc4772
Shihab Hashib <shihabhashib1@gmail.com>
2024-07-09 20:04:32 +0600

    1768. Merge Strings Alternately

---
+++ py_problems/1768.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(1, "../assets/js/pyscript.js")
+
+# ProblemSolving for LeetCode Python Start
+# 1768. Merge Strings Alternately
+
+
+def mergeAlternately(word1, word2):
+    text, i = "", 0
+
+    while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            text += word1[i]
+        if i < len(word2):
+            text += word2[i]
+        i += 1
+
+    return text
+
+# END
+
+
+word1 = "abc"
+word2 = "pqr"
+result = mergeAlternately(word1, word2)
+display(result)
