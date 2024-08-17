70242eafb29fa17ed9f7d5815cb3679222bd6a42
Shihab Hashib <shihabhashib1@gmail.com>
2024-08-17 11:54:07 +0600

    181. Employees Earning More Than Their Managers

---
+++ postgreSQL_problems/181.sql
@@ -0,0 +1,7 @@
+-- ProblemSolving for LeetCode SQL
+-- 181. Employees Earning More Than Their Managers
+
+SELECT e.name AS Employee
+FROM Employee e
+LEFT JOIN Employee m ON m.id = e.managerId
+WHERE e.salary > m.salary
